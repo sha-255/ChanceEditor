d7e89b266f23e65189a60b833c02cc421f12f5b7
sha-255 <fawsgre@mail.ru>
2024-04-20 16:46:53 +0600

    Minor fixes from Asset Store
    Randomizer in package namespace. Asset store tools in gitignore

--- Assets/Plugins/ChanceEditor/Example/Randomizer.cs
+++ Assets/Plugins/ChanceEditor/Example/Randomizer.cs
@@ -1,55 +1,57 @@
-using ChanceEditor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class Randomizer : MonoBehaviour
+namespace ChanceEditor
 {
-    [SerializeField] private List<ChanceWrapper<GameObject>> _chanceObjects;
-
-    private bool _isEveryFrame;
-    private Coroutine _coroutine;
-
-    private void Awake()
+    public class Randomizer : MonoBehaviour
     {
-        _chanceObjects.ForEach(obj => obj.Value.SetActive(false));
-    }
+        [SerializeField] private List<ChanceWrapper<GameObject>> _chanceObjects;
 
-    private void OnDrawGizmos()
-    {
-        _chanceObjects
-            .ForEach(wrapper =>
-                Handles.Label(
-                    wrapper.Value.transform.position,
-                    wrapper.Chance.ToString()
-                    )
-                );
-    }
+        private bool _isEveryFrame;
+        private Coroutine _coroutine;
 
-    public void TrySetActive()
-    {
-        if (_chanceObjects.Count == 0) return;
-        _chanceObjects
-            .ForEach(wrapper => wrapper.Value.SetActive(wrapper.Calculate()));
-    }
-
-    public void SwitchSetActiveEveryFrame()
-    {
-        _isEveryFrame = !_isEveryFrame;
-
-        if (_isEveryFrame)
-            _coroutine = StartCoroutine(SetActiveEveryFrame());
-        else
-            StopCoroutine(_coroutine);
-    }
-
-    private IEnumerator SetActiveEveryFrame()
-    {
-        while (true)
+        private void Awake()
         {
-            TrySetActive();
-            yield return null;
+            _chanceObjects.ForEach(obj => obj.Value.SetActive(false));
+        }
+
+        private void OnDrawGizmos()
+        {
+            _chanceObjects
+                .ForEach(wrapper =>
+                    Handles.Label(
+                        wrapper.Value.transform.position,
+                        wrapper.Chance.ToString()
+                        )
+                    );
+        }
+
+        public void TrySetActive()
+        {
+            if (_chanceObjects.Count == 0) return;
+            _chanceObjects
+                .ForEach(wrapper => wrapper.Value.SetActive(wrapper.Calculate()));
+        }
+
+        public void SwitchSetActiveEveryFrame()
+        {
+            _isEveryFrame = !_isEveryFrame;
+
+            if (_isEveryFrame)
+                _coroutine = StartCoroutine(SetActiveEveryFrame());
+            else
+                StopCoroutine(_coroutine);
+        }
+
+        private IEnumerator SetActiveEveryFrame()
+        {
+            while (true)
+            {
+                TrySetActive();
+                yield return null;
+            }
         }
     }
 }
